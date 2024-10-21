95d733d982516d06b2d5e05f106ba4d9b723db05
DGD666 <841867333@qq.com>
2024-10-21 13:31:54 +0800

    提交

---
+++ Solution11Test.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution11Test {
+
+    Solution11 sol = new Solution11();
+    @org.junit.jupiter.api.Test
+    void threeSum() {
+        int[] a={1,5,12};
+        sol.threeSum( a );
+    }
+}
